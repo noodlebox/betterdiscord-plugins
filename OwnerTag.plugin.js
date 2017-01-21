d2b8ae59a262120fa60c7f883e9b8cf1a2d378c6
noodlebox <ash@noodlebox.moe>
2017-01-21 02:19:01 -0600

    Bump OwnerTag version to 1.3.0

--- OwnerTag.plugin.js
+++ OwnerTag.plugin.js
@@ -352,7 +352,7 @@ var ownerTag = function () {};
     };
 
     ownerTag.prototype.getVersion = function () {
-        return "1.2.4";
+        return "1.3.0";
     };
 
     ownerTag.prototype.getAuthor = function () {
